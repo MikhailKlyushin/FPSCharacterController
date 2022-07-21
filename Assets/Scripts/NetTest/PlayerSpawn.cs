f94a72ef8dfef680c6d9c440485814964465b180
MikhailKlyushin <klyushin.mixail@gmail.com>
2022-07-21 13:02:03 +0500

    Add Scripts/NetTest/PlayerSpawn.cs

--- Assets/Scripts/NetTest/PlayerSpawn.cs
+++ Assets/Scripts/NetTest/PlayerSpawn.cs
@@ -3,8 +3,26 @@ using UnityEngine;
 
 public class PlayerSpawn : NetworkBehaviour
 {
+    [SerializeField] private CharacterConfig _playerConfig;
+    private NetworkObject _spawnedPlayerObject;
+    //private ModelForNetworkPlayer _netModel;
+
     public override void OnNetworkSpawn()
     {
-        Debug.Log("Client Owner player = " + OwnerClientId);
+        if (!IsOwner) return;
+        
+        Debug.Log("Client owner = " + OwnerClientId);
+        _spawnedPlayerObject = NetworkManager.Singleton.SpawnManager.GetPlayerNetworkObject(OwnerClientId);
+        var playerObject = _spawnedPlayerObject.gameObject;
+
+        //var ownerId = playerObject.GetComponent<ClientIsOwner>().OwnerId;
+
+        //var cameraView = _playerService.CreatePlayerCamera(playerObject.transform);
+        var model = new CharacterModel(new InputKeyAndMouse(), _playerConfig);
+        Debug.Log("Model ID = " + model.ID);
+        var view = playerObject.GetComponent<CharacterView>();
+        view.SetModel(model);
+        Debug.Log("View ID = " + view.ID);
+
     }
 }
