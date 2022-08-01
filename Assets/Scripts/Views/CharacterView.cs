bba4e8caea968db11d09d8ebb9ff602f28ba0fd6
MikhailKlyushin <klyushin.mixail@gmail.com>
2022-08-01 14:24:27 +0500

    Fixed network interaction via NetworkVariable

--- Assets/Scripts/Views/CharacterView.cs
+++ Assets/Scripts/Views/CharacterView.cs
@@ -23,13 +23,13 @@ public class CharacterView : NetworkBehaviour, IIdentified
     private float _directionVerticalClient;
 
     private NetworkVariable<Vector3> _velocity = new NetworkVariable<Vector3>(default, 
-        NetworkVariableReadPermission.Owner, NetworkVariableWritePermission.Owner);
+        NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
     private NetworkVariable<Quaternion> _rotate = new NetworkVariable<Quaternion>(default, 
-        NetworkVariableReadPermission.Owner, NetworkVariableWritePermission.Owner);
+        NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
     private NetworkVariable<float> _directionHorizontal = new NetworkVariable<float>(default, 
-        NetworkVariableReadPermission.Owner, NetworkVariableWritePermission.Owner);
+        NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
     private NetworkVariable<float> _directionVertical = new NetworkVariable<float>(default, 
-        NetworkVariableReadPermission.Owner, NetworkVariableWritePermission.Owner);
+        NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
     
 
     private readonly int _horizontal = Animator.StringToHash("Horizontal");
@@ -44,9 +44,7 @@ public class CharacterView : NetworkBehaviour, IIdentified
 
     public void SetModel(CharacterModel model)
     {
-        if (IsClient)
-        {
-            _model = model;
+        _model = model;
 
             _characterID = model.ID;
 
@@ -58,35 +56,18 @@ public class CharacterView : NetworkBehaviour, IIdentified
 
             _model.Velocity.Subscribe(velocity => { _velocityClient = velocity; }).AddTo(_disposables);
             _model.RotateY.Subscribe(rotate => { _rotateClient = rotate; }).AddTo(_disposables);
-        }
-        else
-        {
-            _model = new CharacterNetworkModel();
-        }
-
+        
+        
         Observable.EveryFixedUpdate().Subscribe(_ =>
         {
-            if (IsServer && IsOwner)
+            if (IsOwner)
             {
                 _velocity.Value = _velocityClient;
                 _rotate.Value = _rotateClient;
                 _directionHorizontal.Value = _directionHorizontalClient;
                 _directionVertical.Value = _directionVerticalClient;
             }
-            else if (IsClient && IsOwner)
-            {
-                _velocity.OnValueChanged?.Invoke(_velocity.Value, _velocityClient);
-                _rotate.OnValueChanged?.Invoke(_rotate.Value, _rotateClient);
-                _directionHorizontal.OnValueChanged?.Invoke(_directionHorizontal.Value, _directionHorizontalClient);
-                _directionVertical.OnValueChanged?.Invoke(_directionVertical.Value, _directionVerticalClient);
-                _velocity.Value = _velocityClient;
-                _rotate.Value = _rotateClient;
-                _directionHorizontal.Value = _directionHorizontalClient;
-                _directionVertical.Value = _directionVerticalClient;
-                //UpdateClientPositionAndRotation(_velocityClient, _rotateClient);
-                //UpdateClientAnimator(_directionHorizontalClient, _directionVerticalClient);
-            }
-            
+
             _rigidbody.velocity = transform.TransformDirection(_velocity.Value);
             Debug.DrawRay(transform.position, _velocity.Value, Color.green);
             
@@ -110,17 +91,5 @@ public class CharacterView : NetworkBehaviour, IIdentified
         _animator.SetFloat(_horizontal, _directionHorizontal.Value);
         _animator.SetFloat(_vertical, _directionVertical.Value);
         _animator.speed = 3f; //_model.MoveSpeed;   TODO: rewrite via config
-    }
-    
-    private void UpdateClientPositionAndRotation(Vector3 newPosition, Quaternion newRotate)
-    {
-        _velocity.Value = newPosition;
-        _rotate.Value = newRotate;
-    }
-    
-    private void UpdateClientAnimator(float newHorizontal, float newVertical)
-    {
-        _directionHorizontal.Value = newHorizontal;
-        _directionVertical.Value = newVertical;
     }
 }
